afd6e12959c0c817606fd9e11d7d383b5d214d5f
afffuck <gerovattack@gmail.com>
2025-08-22 14:50:33 +0300

    Update uninstall-kaliify.sh

--- uninstall-kaliify.sh
+++ uninstall-kaliify.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # ==========================================================
 #  Kaliify-Termux Uninstaller
-#  License: MIT
+#  License: Apache-2.0
 # ==========================================================
 
 set -e
